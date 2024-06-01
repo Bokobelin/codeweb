6ede85f61443fa1838aa07947ceb1046bd0af460
Bokobelin <matthieu.richelle@gmail.com>
2024-06-01 09:07:20 +0200

    Add script functionnality

---
+++ script.lua
@@ -0,0 +1,8 @@
+local runButton = get("runButton")
+local codeInput = get("codeInput")
+local outputDiv = get("output")
+
+runButton.on_click(function()
+  local code = codeInput.get_content()
+  outputDiv.set_content(code)
+end)
