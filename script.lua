159200aa0a13e6aef0265f5d8a2ca4059a81d880
Bokobelin <matthieu.richelle@gmail.com>
2024-06-01 09:20:20 +0200

    Try to make the script work

--- script.lua
+++ script.lua
@@ -2,7 +2,7 @@ local runButton = get("runButton")
 local codeInput = get("codeInput")
 local outputDiv = get("output")
 
-runButton.on_click(function()
+runButton.on_click(function(
   local code = codeInput.get_content()
   outputDiv.set_content(code)
-end)
+)end)
